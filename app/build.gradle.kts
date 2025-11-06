8487bd03367ae375912b617e929575cb400c259b
sneyker408 <sneyker408@hotmail.com>
2025-11-06 17:24:19 -0600

    agregandole estilo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //loginxml
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+
 }
